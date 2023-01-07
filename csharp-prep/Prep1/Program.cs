a7d0c2a70d4bdd307e7d245eaaf6714057ff9b16
bykarol <brachokarol.sud@gmail.com>
2023-01-08 00:33:15 +0100

    Learning activity prep1 DONE

--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -2,8 +2,17 @@ using System;
 
 class Program
 {
-    static void Main(string[] args)
-    {
-        Console.WriteLine("Hello Prep1 World!");
-    }
+  static void Main(string[] args)
+  {
+    Console.WriteLine("Hello Prep1 World!");
+    Console.WriteLine("************************");
+
+    Console.Write("What is your first name? ");
+    string userName = Console.ReadLine();
+
+    Console.Write("What is your last name? ");
+    string userLastname = Console.ReadLine();
+
+    Console.WriteLine($"Your name is {userLastname}, {userName} {userLastname}.");
+  }
 }
