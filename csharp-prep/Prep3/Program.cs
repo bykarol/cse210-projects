0b552ebece7568806ec17e7a1a5a0b393e32d488
bykarol <brachokarol.sud@gmail.com>
2023-01-09 17:33:16 +0100

    Learning actv prep 3 section 3 DONE

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -6,9 +6,11 @@ class Program
   {
     Console.WriteLine("Hello Prep3 World!");
     Console.WriteLine("*************************");
-    Console.Write("What is the magic number? ");
-    string userInput = Console.ReadLine();
-    int magicNum = int.Parse(userInput);
+    // Console.Write("What is the magic number? ");
+    // string userInput = Console.ReadLine();
+    // int magicNum = int.Parse(userInput);
+    Random randomGenerator = new Random();
+    int magicNum = randomGenerator.Next(1, 11);
     int guessNum;
     do
     {
