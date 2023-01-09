fdfd5cfea10acab221332234726f5b8b5ef4209a
bykarol <brachokarol.sud@gmail.com>
2023-01-09 17:50:56 +0100

    Stretch challenge 2 - prep3 learning actv done

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -18,10 +18,26 @@ class Program
       Console.Write("What is your guess? ");
       string guessInput = Console.ReadLine();
       guessNum = int.Parse(guessInput);
+      counter++;
 
       if (magicNum == guessNum)
       {
         Console.WriteLine("You guessed it!!");
+        Console.WriteLine($"You made {counter} attemps.");
+        Console.WriteLine("***********************");
+        Console.Write("Do you want to play again? (yes or no) ");
+        string playAgain = Console.ReadLine();
+        if (playAgain == "yes")
+        {
+          magicNum = randomGenerator.Next(1, 11);
+          counter = 0;
+        }
+        else
+        {
+          Console.WriteLine("Come back soon! Bye bye!!");
+        }
+
+
       }
       else if (magicNum < guessNum)
       {
@@ -31,9 +47,8 @@ class Program
       {
         Console.WriteLine("Higher");
       }
-      counter++;
     } while (magicNum != guessNum);
-    Console.WriteLine($"You made {counter} attemps.");
+
 
 
 
