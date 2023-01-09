b21cd2af03cb288233b4e8800adf8bdf92a0bd0f
bykarol <brachokarol.sud@gmail.com>
2023-01-09 17:29:02 +0100

    learning actv prep 3 section 2 - DONE

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -6,27 +6,29 @@ class Program
   {
     Console.WriteLine("Hello Prep3 World!");
     Console.WriteLine("*************************");
-
     Console.Write("What is the magic number? ");
     string userInput = Console.ReadLine();
     int magicNum = int.Parse(userInput);
+    int guessNum;
+    do
+    {
+      Console.Write("What is your guess? ");
+      string guessInput = Console.ReadLine();
+      guessNum = int.Parse(guessInput);
 
-    Console.Write("What is your guess? ");
-    string guessInput = Console.ReadLine();
-    int guessNum = int.Parse(guessInput);
-
-    if (magicNum == guessNum)
-    {
-      Console.WriteLine("You guessed it!!");
-    }
-    else if (magicNum < guessNum)
-    {
-      Console.WriteLine("Lower");
-    }
-    else
-    {
-      Console.WriteLine("Higher");
-    }
+      if (magicNum == guessNum)
+      {
+        Console.WriteLine("You guessed it!!");
+      }
+      else if (magicNum < guessNum)
+      {
+        Console.WriteLine("Lower");
+      }
+      else
+      {
+        Console.WriteLine("Higher");
+      }
+    } while (magicNum != guessNum);
 
 
 
