cda1c6fdc69d11bf63e68c300bc6e52886bde30f
bykarol <brachokarol.sud@gmail.com>
2023-01-11 22:39:01 +0100

    welcome message function done

--- csharp-prep/Prep5/Program.cs
+++ csharp-prep/Prep5/Program.cs
@@ -2,8 +2,17 @@ using System;
 
 class Program
 {
-    static void Main(string[] args)
-    {
-        Console.WriteLine("Hello Prep5 World!");
-    }
+  static void Main(string[] args)
+  {
+    Console.WriteLine("Hello Prep5 World!");
+    Console.WriteLine("************************");
+
+    Console.WriteLine(DisplayMessage());
+  }
+
+  static string DisplayMessage()
+  {
+    string welcomeMessage = "Welcome to the Program!";
+    return welcomeMessage;
+  }
 }
