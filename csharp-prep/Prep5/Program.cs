24d0add4f2bea9462c1968e547a3e7a45f5ff621
bykarol <brachokarol.sud@gmail.com>
2023-01-11 23:22:14 +0100

    learning actv 5 functions and methods done

--- csharp-prep/Prep5/Program.cs
+++ csharp-prep/Prep5/Program.cs
@@ -7,12 +7,40 @@ class Program
     Console.WriteLine("Hello Prep5 World!");
     Console.WriteLine("************************");
 
-    Console.WriteLine(DisplayMessage());
+    DisplayMessage();
+    string name = PromptUserName();
+    int number = PromptUserNumber();
+    int squareNum = SquareNumber(number);
+    DisplayResult(name, squareNum);
   }
 
-  static string DisplayMessage()
+  static void DisplayMessage()
   {
-    string welcomeMessage = "Welcome to the Program!";
-    return welcomeMessage;
+    Console.WriteLine("Welcome to the Program!");
   }
+  static string PromptUserName()
+  {
+    Console.Write("Please enter your name: ");
+    string name = Console.ReadLine();
+    return name;
+  }
+  static int PromptUserNumber()
+  {
+    Console.Write("Please enter your favorite number: ");
+    int favNum = int.Parse(Console.ReadLine());
+    return favNum;
+  }
+  static int SquareNumber(int favNum) //double favNum
+  {
+    int squareNum = favNum * favNum; //this code is more according to the requirements
+    return squareNum;
+    // double squareNum = Math.Pow(favNum, 2);
+    // int squareNumInt = (int)squareNum;
+    // return squareNumInt;
+  }
+  static void DisplayResult(string name, int num)
+  {
+    Console.WriteLine($"{name}, the square of your number is {num}");
+  }
+
 }
