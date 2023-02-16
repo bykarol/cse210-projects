1512cf9e613ccc6cb4e9f08e554ebe0706c4a906
brachokarol <brachokarol.sud@gmail.com>
2023-02-16 11:21:47 +0100

    week 7 Inheritance Learning Activity

--- prepare/Learning04/Program.cs
+++ prepare/Learning04/Program.cs
@@ -4,6 +4,17 @@ class Program
 {
   static void Main(string[] args)
   {
-    Console.WriteLine("Hello Learning04 World!");
+    Console.WriteLine("Assignments - inheritance");
+
+    Assignment assignment1 = new Assignment("Karol", "Inheritance");
+    Console.WriteLine(assignment1.GetSummary());
+    Console.WriteLine("Testing GetSummary() and GetHomeworkList() methods (no using parent class getters)");
+    MathAssignment assignment2 = new MathAssignment("Roberto Rodríguez", "Fractions", "Section 7.3", "Problems 8-19");
+    Console.WriteLine(assignment2.GetSummary()); // Testing GetSummary() from base class
+    Console.WriteLine(assignment2.GetHomeworkList()); // Testing GetHomeworkList() from MathAssignment class
+    Console.WriteLine("Testing GetWritingInfo() using getters from parent class");
+    WritingAssignment assignment3 = new WritingAssignment("Mary Waters", "European History", "The Causes of World War II");
+    Console.WriteLine(assignment3.GetSummary());
+    Console.WriteLine(assignment3.GetWritingInfo());
   }
 }
