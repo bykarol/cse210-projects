486c6562558c99e72664e515a80b2f3a2de087ca
brachokarol <brachokarol.sud@gmail.com>
2023-03-02 13:22:45 +0100

    using polymorphism class shape and abstract method getArea()

--- prepare/Learning05/Program.cs
+++ prepare/Learning05/Program.cs
@@ -2,8 +2,61 @@ using System;
 
 class Program
 {
-    static void Main(string[] args)
+  static void Main(string[] args)
+  {
+    List<Shape> shapes = new List<Shape>();
+
+    Square sq = new Square("yellow", 4);
+    shapes.Add(sq);
+    Rectangle rec = new Rectangle("blue", 10, 5);
+    shapes.Add(rec);
+    Circle circ = new Circle("red", 7);
+    shapes.Add(circ);
+
+    // DisplayShapeInfo(sq);
+    // DisplayShapeInfo(rec);
+    // DisplayShapeInfo(circ);
+
+    foreach (Shape shape in shapes)
     {
-        Console.WriteLine("Hello Learning05 World!");
+      string shapeName = "";
+      string color = shape.GetColor();
+      double area = shape.GetArea();
+
+      switch (color)
+      {
+        case "yellow":
+          shapeName = "Square";
+          break;
+        case "blue":
+          shapeName = "Rectangle";
+          break;
+        case "red":
+          shapeName = "Circle";
+          break;
+      }
+      Console.WriteLine($"The {color} {shapeName} area is {area:f2} cm2");
     }
+
+  }
+  //   public static void DisplayShapeInfo(Shape shape)
+  //   {
+  //     string shapeName = "";
+  //     string color = shape.GetColor();
+  //     double area = shape.GetArea();
+  //     switch (color)
+  //     {
+  //       case "yellow":
+  //         shapeName = "Square";
+  //         break;
+  //       case "blue":
+  //         shapeName = "Rectangle";
+  //         break;
+  //       case "red":
+  //         shapeName = "Circle";
+  //         break;
+  //     }
+  //     Console.WriteLine($"The {color} {shapeName} area is {area:f2} cm2");
+  //   }
+
 }
