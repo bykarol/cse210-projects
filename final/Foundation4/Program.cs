5b0ff82f4b2032bf0bc190d25b44137e44413787
brachokarol <brachokarol.sud@gmail.com>
2023-03-24 14:08:49 +0100

    final project My Foundation 4 - exercise tracking COMPLETED

--- final/Foundation4/Program.cs
+++ final/Foundation4/Program.cs
@@ -1,9 +1,26 @@
-using System;
-
 class Program
 {
-    static void Main(string[] args)
+  static void Main(string[] args)
+  {
+    Console.WriteLine("EXERCISE TRACKING");
+    Console.WriteLine("******************");
+    List<Activity> activities = new List<Activity>();
+    //Creating the three types of activities
+    RunningActivity runnningActv = new RunningActivity("Running", "23 Jan 2023", 30, 3.0);
+    CyclingActivity cyclingActv = new CyclingActivity("Cycling", "23 Feb 2023", 90, 6.5);
+    SwimmingActivity swimmActv = new SwimmingActivity("Swimming", "23 Mar 2023", 60, 50);
+    //Adding the activities to the list of Activities.
+    activities.Add(runnningActv);
+    activities.Add(cyclingActv);
+    activities.Add(swimmActv);
+
+    foreach (Activity actv in activities)
     {
-        Console.WriteLine("Hello Foundation4 World!");
+      actv.displaySummary();
+      Console.WriteLine("******************");
+
     }
+
+
+  }
 }
